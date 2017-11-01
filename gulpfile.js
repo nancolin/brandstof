f1e6ded7b33be892689aa16046f050c166b01b94
Jsilvermist <jsilvermist@silverlinkz.net>
2017-11-01 13:55:28 -0400

    Use gulp build script with src directory structure

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const del = require('del');
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const fileInclude = require('gulp-file-include');
+const htmlMinifier = require('gulp-html-minifier');
+const babelMinify = require("gulp-babel-minify");
+const cssSlam = require('css-slam').gulp;
+
+gulp.task('build', function() {
+  return del(['build'])
+  .then(() => {
+    gulp.src([
+      'src/**/*',
+      '!src/_fragments',
+      '!src/_fragments/**'
+    ])
+    .pipe(gulpif(/\.html$/, fileInclude({
+      prefix: '@@',
+      basepath: './src/'
+    })))
+    .pipe(gulpif(/\.html$/, htmlMinifier({
+      collapseWhitespace: true
+    })))
+    .pipe(gulpif(/\.html|.css$/, cssSlam()))
+    .pipe(gulpif(/\.js$/, babelMinify({
+      mangle: {
+        keepClassName: true
+      }
+    })))
+    .pipe(gulp.dest('build/'));
+  });
+});
+
+gulp.task('default', ['build']);
